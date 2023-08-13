b1880207815dacaeb52bbf9be69f352e9d2dd331
dyeghiaz <dyeghiaz@gmail.com>
2023-08-13 22:05:36 +0400

    initial commit

---
+++ src/main/java/com/aca/acaonlinewallet/AcaOnlineWalletApplication.java
@@ -0,0 +1,13 @@
+package com.aca.acaonlinewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaOnlineWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcaOnlineWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aca/acaonlinewallet/AcaOnlineWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aca.acaonlinewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaOnlineWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
