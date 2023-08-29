0ed350926614f65af70f571a385e0d51ecf97d34
Artur Stepanyan <artur.stepanyan@bureauveritas.com>
2023-08-29 18:41:10 +0400

    Update Entity Structure

--- src/main/java/com/aca/acaonlinewallet/entity/Wallet.java
+++ src/main/java/com/aca/acaonlinewallet/entity/Wallet.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
 @Table(name = "wallet", schema = "wallet_v1")
 @Getter
@@ -21,11 +19,11 @@ public class Wallet {
     @Column(name = "balance")
     private Double balance;
 
+    @Column(name = "number")
+    private String number;
+
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL)
-    private List<Card> listOfCard;
 
 }
