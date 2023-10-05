2b0db0cc49ce4d9a26762aac682a8e2fd999ca4d
serobkirakosyan <serob.kirakosya.y@gmail.com>
2023-10-05 21:28:54 +0400

    ChangingDefaultCard

--- src/main/java/com/aca/acaonlinewallet/AcaOnlineWalletApplication.java
+++ src/main/java/com/aca/acaonlinewallet/AcaOnlineWalletApplication.java
@@ -3,11 +3,13 @@ package com.aca.acaonlinewallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class AcaOnlineWalletApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcaOnlineWalletApplication.class, args);
-    }
 
+    }
 }
